96fb831dab0fce805484d420694e0d1ab0ed57d9
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-29 19:24:45 -0300

    ex05 added

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 12:41:47 by jarao-de          #+#    #+#             */
+/*   Updated: 2024/08/29 19:15:14 by jarao-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		index = index + 1;
+	}
+	return (index);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	index;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+
+	index = 0;
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size <= dest_len)
+		return (size + src_len);
+	while (src[index] != '\0' && index <= size - dest_len - 1)
+	{
+		dest[dest_len + index] = src[index];
+		index = index + 1;
+	}
+	dest[dest_len + index] = '\0';
+	return (dest_len + src_len);
+}
